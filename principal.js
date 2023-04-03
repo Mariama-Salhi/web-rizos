1cb9647871044d3467b064d648cd29f830ecd1fa
Mariama <merysalhi32@gmail.com>
2023-04-03 09:54:11 +0200

    hola

---
+++ principal.js
@@ -0,0 +1,29 @@
+$('.box').click(function() {
+    $(this).toggleClass('clicked');
+  });
+  var cuadro1 = document.getElementById("cuadro1");
+  cuadro1.style.top = "50px";
+  cuadro1.style.left = "100px";
+  
+  var cuadro2 = document.getElementById("cuadro2");
+  cuadro2.style.top = "100px";
+  cuadro2.style.left = "200px";
+  
+  var cuadro3 = document.getElementById("cuadro3");
+  cuadro3.style.top = "150px";
+  cuadro3.style.left = "300px";
+ 
+
+
+
+
+
+  
+
+
+
+
+
+  
+  
+  
